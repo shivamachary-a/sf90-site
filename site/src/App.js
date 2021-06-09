d90eaeaf8ad23e4c6975b996140bfef5681d4c87
shivamachary-a <62761042+shivamachary-a@users.noreply.github.com>
2021-06-09 22:45:32 +0100

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -19,13 +19,7 @@ function App() {
               <img style={{ height: "auto", width: "15vw", position: "relative", left: "-5vw" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852264856634916874/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-09_at_20.15.28.png?width=578&height=1251"></img>
               <VStack>
                 <Heading>meet SF90</Heading>
-                <Text>save money, spend freely and invest, with our tech-first, cloud based challenger bank and trading services.</Text>
-                <Text>physical branches and unreliable online services are a thing of the past.</Text>
-                <Text>and for that matter, banks making money off of unethical and unsustainable investments is not that great either.</Text>
-                <Text>whether you're a day trader, quant, developer, or just want to invest some money for your future;</Text>
-                <Text>whether you're trading stocks, futures, options, crytocurrencies or etfs;</Text>
-                <Text>or if you just want to put some money aside and earn interest from from ethical investments we make with our cutting edge AI and data science technology;</Text>
-                <Text fontWeight="bold">we've got you</Text>
+
                 <Text>the first complete financial ecosystem built from the ground up with a focus on sustainability, ethics, and community.</Text>
                 <Heading>coming soon.</Heading>
               </VStack>
