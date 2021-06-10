db4031c4dcebe1081b9701d327418d1233154bf2
Shivam Acharya <shivam@shivams-mbp.home>
2021-06-10 18:39:48 +0100

    revamp

---
+++ site/src/Home.js
@@ -0,0 +1,40 @@
+import { isMobile } from "react-device-detect";
+import { Grid, Box, VStack, HStack, Heading, ChakraProvider } from '@chakra-ui/react';
+
+function Home() {
+    if (!isMobile) {
+        return (
+            <ChakraProvider>
+                <Box bg="linear-gradient(90deg, rgba(55,11,194,0.7) 0%, rgba(9,43,254,0.8) 100%);"
+                    backdropFilter="blur(18px)"
+                >
+                    <Grid templateColumns="repeat(2, 1fr)">
+                        <Box textAlign="center" fontSize="xl" position="relative" display="flex" justifyContent="center" alignItems="center" h="100vh" w="50vw" maxWidth="50vw">
+                            <VStack>
+                                <HStack>
+                                    <img style={{ height: "auto", width: "20vw", position: "relative", boxShadow: "box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );" }} alt="" src="https://i.ibb.co/89dpvVX/Rotato-Snapshot-Black.png"></img>
+
+                                    <img style={{ height: "auto", width: "20vw", position: "relative", left: "-10vw", boxShadow: "box-shadow: 0 8px 32px 0 rgba( 31, 38, 135, 0.37 );" }} alt="" src="https://i.ibb.co/rHfLZwc/Rotato-Snapshot-White.png"></img>
+                                </HStack>
+                            </VStack>
+                        </Box>
+                        <Box p="2em" display="flex" justifyContent="start" alignItems="center" w="100%">
+                            <VStack alignItems="start">
+                                <Heading size="3xl" color="white" style={{ textAlign: "left" }}>
+                                    Meet SF90, the UK's first complete financial ecosystem built from the ground up with a focus on sustainability, ethics and community.
+                                </Heading>
+
+                            </VStack>
+                        </Box>
+
+                    </Grid>
+
+                </Box>
+            </ChakraProvider>
+
+
+        )
+    }
+}
+
+export default Home;
--- site/src/index.js
+++ site/src/index.js
@@ -8,7 +8,7 @@ import * as serviceWorker from './serviceWorker';
 ReactDOM.render(
   <StrictMode>
     <ColorModeScript />
-    <App />
+    <App></App>
   </StrictMode>,
   document.getElementById('root')
 );
