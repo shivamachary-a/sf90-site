6c56210f018de64bd20d70992e87b0b1f149952b
shivamachary-a <62761042+shivamachary-a@users.noreply.github.com>
2021-06-10 15:58:20 +0100

    Update App.js

--- site/src/App.js
+++ site/src/App.js
@@ -15,8 +15,8 @@ function App() {
 
             <HStack>
               <img style={{ height: "auto", width: "20vw", position: "relative", left: "-5vw" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
-              <img style={{ height: "auto", width: "15vw", position: "relative" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534965886910484/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.59.png?width=578&height=1251"></img>
-              <img style={{ height: "auto", width: "15vw", position: "relative", left: "-5vw" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534963898548304/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.56.png?width=578&height=1251"></img>
+              <img style={{ height: "auto", width: "15vw", position: "relative" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534965886910484/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.59.png"></img>
+              <img style={{ height: "auto", width: "15vw", position: "relative", left: "-5vw" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534963898548304/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.56.png"></img>
               <VStack>
                 <Heading>meet SF90</Heading>
                 <Text>the first complete financial ecosystem built from the ground up with a focus on sustainability, ethics, and community.</Text>
@@ -35,14 +35,14 @@ function App() {
           <Box textAlign="center" fontSize="xl" position="relative" display="flex" justifyContent="left" alignItems="left" p="10vw" h="100vh" w="100vw">
 
             <VStack>
-              <img style={{ height: "auto", width: "100vw", position: "relative" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534963898548304/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.56.png?width=578&height=1251"></img>
+              <img style={{ height: "auto", width: "100vw", position: "relative" }} alt="" src="https://cdn.discordapp.com/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
               <VStack>
                 <Heading>meet SF90</Heading>
                 <Text>the first complete financial ecosystem built from the ground up with a focus on sustainability, ethics, and community.</Text>
                 <Heading>coming soon.</Heading>
               </VStack>
-              <img style={{ height: "auto", width: "100vw", position: "relative" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534965886910484/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.59.png?width=578&height=1251"></img>
-              <img style={{ height: "auto", width: "100vw", position: "relative", top: "-60vh" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534963898548304/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.56.png?width=578&height=1251"></img>
+              <img style={{ height: "auto", width: "100vw", position: "relative" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534965886910484/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.59.png"></img>
+              <img style={{ height: "auto", width: "100vw", position: "relative", top: "-60vh" }} alt="" src="https://media.discordapp.net/attachments/692861972731002995/852534963898548304/Simulator_Screen_Shot_-_iPhone_12_Pro_Max_-_2021-06-10_at_14.07.56.png"></img>
             </VStack>
           </Box>
         </VStack>
