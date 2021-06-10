36423db58a9dde33c23dcf6e3ba1c010a7a6c676
Shivam Acharya <shivam@shivams-mbp.home>
2021-06-10 19:00:32 +0100

    revamp4

--- site/src/components/navbar.js
+++ site/src/components/navbar.js
@@ -1,11 +1,10 @@
-import { Box, Flex, Heading } from "@chakra-ui/layout";
+import { Box, Flex } from "@chakra-ui/layout";
 
 const NavBar = () => {
 	return (
 		<Box>
 			<Flex justifyContent="center" alignItems="center" p="0.5em" maxH="10vh">
 				<img alt="logo" style={{ height: "6vh", width: "auto" }} src="https://cdn.discordapp.com/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
-				<Heading>SF90</Heading>
 			</Flex>
 		</Box>
 	);
