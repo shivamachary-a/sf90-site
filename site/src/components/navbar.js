9560bee704d721496744c0811103a5c783e09835
Shivam Acharya <shivam@shivams-mbp.home>
2021-06-10 18:46:12 +0100

    revamp2

--- site/src/components/navbar.js
+++ site/src/components/navbar.js
@@ -4,7 +4,7 @@ const NavBar = () => {
 	return (
 		<Box>
 			<Flex justifyContent="center" alignItems="center" p="0.5em" maxH="10vh">
-				<img style={{ height: "6vh", width: "auto" }} src="https://cdn.discordapp.com/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
+				<img alt="logo" style={{ height: "6vh", width: "auto" }} src="https://cdn.discordapp.com/attachments/692861972731002995/852270505528655912/output-onlinepngtools_1.png"></img>
 				<Heading>SF90</Heading>
 			</Flex>
 		</Box>
